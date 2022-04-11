e00bacde6c06f9a370cb01a6a6a4e3f723672de3
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-11 19:58:43 -0400

    Installed Faker and created a component for comments.

---
+++ src/CommentDetail.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import faker from "@faker-js/faker"
+
+const CommentDetail = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={faker.image.image(128, 128)} alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Lucky
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  )
+}
+
+export default CommentDetail
--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import faker from "@faker-js/faker"
+import CommentDetail from "./CommentDetail"
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Lucky
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   )
 }
