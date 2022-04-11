8fe2d7c77adcc29a324234f86b33ed3840106a4e
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-11 01:26:09 -0400

    Created index.js to end Section 1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return <div>Hello bbs :)</div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
