239d86b396b16b7217b2fdf0fafe2a69b1d4c5b5
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-11 20:05:21 -0400

    Intro to props

--- src/CommentDetail.jsx
+++ src/CommentDetail.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import faker from "@faker-js/faker"
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
@@ -9,7 +9,7 @@ const CommentDetail = () => {
       </a>
       <div className="content">
         <a href="/" className="author">
-          Lucky
+          {props.author}
         </a>
         <div className="metadata">
           <span className="date">Today at 6:00PM</span>
