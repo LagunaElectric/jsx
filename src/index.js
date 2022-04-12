d9b45c080d4c397ad3afe6c2a335218d7126cc65
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-11 20:33:20 -0400

    Created the Approval Card and tested it

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import faker from "@faker-js/faker"
 import CommentDetail from "./CommentDetail"
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo="Today at 9:37AM"
